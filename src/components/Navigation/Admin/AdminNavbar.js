7b39b2791cdd24f8e60742d2e44eeca9b677e71b
Ahmad Ajaz <er.malikajaz26@gmail.com>
2022-10-07 19:29:52 +0530

    Deploy V1

--- src/components/Navigation/Admin/AdminNavbar.js
+++ src/components/Navigation/Admin/AdminNavbar.js
@@ -88,7 +88,7 @@ const AdminNavbar = ({ isLogin }) => {
                       className="-ml-1 mr-2 h-5 w-5"
                       aria-hidden="true"
                     />
-                    <span>New Post</span>
+                    <span>New Blog</span>
                   </Link>
                   {/* Logout */}
                   <button
